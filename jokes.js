134eb43a6196d3219284b0c6b4cf2f75c1a72686
Mahnoor Bibi <56926453+NoorNoor19@users.noreply.github.com>
2021-12-08 17:00:24 -0500

    Create jokes.js

---
+++ jokes.js
@@ -0,0 +1,17 @@
+
+const joke = document.querySelector('.joke');
+const btn = document.getElementById('btn');
+const jokeParagraph = document.querySelector('.joke p');
+
+// Getting data from the Api using Fetch Method
+btn.addEventListener('click',getRandom);
+getRandom();
+async function getRandom(){
+  const jokeRes = await fetch('https://icanhazdadjoke.com/', {
+    headers: {
+      'Accept': 'application/json'
+    }
+  });
+  const resJson = await jokeRes.json();
+  jokeParagraph.innerHTML = resJson.joke;
+}
